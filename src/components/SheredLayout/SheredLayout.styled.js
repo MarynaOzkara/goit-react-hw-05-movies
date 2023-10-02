9dae23ab17e9891797b0eadcd36335a73645e0ec
MarynaOzkara <ozkaramaryna@gmail.com>
2023-10-02 15:13:08 +0300

    final fix

--- src/components/SheredLayout/SheredLayout.styled.js
+++ src/components/SheredLayout/SheredLayout.styled.js
@@ -9,7 +9,7 @@ export const Container = styled.div`
 export const Header = styled.header`
   width: 100%;
   padding: 10px 0;
-  padding-left: 50px;
+  padding-left: 100px;
   background-color: tomato;
   box-shadow: 0px 2px 4px -1px rgba(0, 0, 0, 0.2),
     0px 4px 5px 0px rgba(0, 0, 0, 0.14), 0px 1px 10px 0px rgba(0, 0, 0, 0.12);
